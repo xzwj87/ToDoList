37879005b68eb968948143f44053903ff0a11415
nex3z <litianxing9@gmail.com>
2016-03-01 22:33:46 +0800

    Decouple UI and logic for AddScheduleActivity.

---
+++ app/src/main/java/com/github/xzwj87/todolist/schedule/presenter/presenter.java
@@ -0,0 +1,9 @@
+package com.github.xzwj87.todolist.schedule.presenter;
+
+public interface presenter {
+    void resume();
+
+    void pause();
+
+    void destroy();
+}
--- app/src/main/java/com/github/xzwj87/todolist/schedule/ui/AddScheduleView.java
+++ app/src/main/java/com/github/xzwj87/todolist/schedule/ui/AddScheduleView.java
@@ -1,17 +1,20 @@
 package com.github.xzwj87.todolist.schedule.ui;
 
+import android.content.Context;
+
 public interface AddScheduleView {
 
     void showPickStartDateDlg(int year, int monthOfYear, int dayOfMonth);
-    void showPickStartTimeDlg(int hourOfDay, int minute, int second);
     void showPickEndDateDlg(int year, int monthOfYear, int dayOfMonth);
+    void showPickStartTimeDlg(int hourOfDay, int minute, int second);
     void showPickEndTimeDlg(int hourOfDay, int minute, int second);
 
-    void onStartDateSet(int year, int monthOfYear, int dayOfMonth);
-    void onStartTimeSet(int hourOfDay, int minute, int second);
-    void onEndDateSet(int year, int monthOfYear, int dayOfMonth);
-    void onEndTimeSet(int hourOfDay, int minute, int second);
+    void updateStartDateDisplay(String startDate);
+    void updateEndDateDisplay(String endDate);
+    void updateStartTimeDisplay(String startTime);
+    void updateEndTimeDisplay(String endTime);
 
-    void onSave();
-
+    String getScheduleTitle();
+    // TODO: Inject context to presenter
+    Context getViewContext();
 }
