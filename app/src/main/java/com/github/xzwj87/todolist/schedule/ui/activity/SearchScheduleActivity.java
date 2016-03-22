5dfa91d54c25f6a5881deb46e0445f35e07f1ba0
nex3z <litianxing9@gmail.com>
2016-03-22 21:27:23 +0800

    Expand search view in SearchScheduleActivity by default.

--- app/src/main/java/com/github/xzwj87/todolist/schedule/ui/activity/SearchScheduleActivity.java
+++ app/src/main/java/com/github/xzwj87/todolist/schedule/ui/activity/SearchScheduleActivity.java
@@ -1,21 +1,32 @@
 package com.github.xzwj87.todolist.schedule.ui.activity;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.github.xzwj87.todolist.R;
+
+import java.lang.reflect.Field;
 
 public class SearchScheduleActivity extends AppCompatActivity {
     private static String LOG_TAG = SearchScheduleActivity.class.getSimpleName();
 
     public static final String QUERY = "search_keyword";
 
-    private String mKeyword;
+    private String mQuery;
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +37,40 @@ public class SearchScheduleActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
-            mKeyword = getIntent().getStringExtra(QUERY);
-            Log.v(LOG_TAG, "onCreate(): mKeyword = " + mKeyword);
+            mQuery = getIntent().getStringExtra(QUERY);
+            Log.v(LOG_TAG, "onCreate(): mQuery = " + mQuery);
+        }
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_search_schedule, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchItem.expandActionView();
+        mSearchView.setQuery(mQuery, false);
+        mSearchView.clearFocus();
+
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            mQuery = intent.getStringExtra(SearchManager.QUERY);
+            Log.v(LOG_TAG, "handleIntent(): mQuery = " + mQuery);
+
         }
     }
 
