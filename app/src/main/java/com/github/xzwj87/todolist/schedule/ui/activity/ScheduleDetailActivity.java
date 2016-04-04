3df6988e2280135a4299b145543490dbad8d08e4
JasonWang <xzwj87@gmail.com>
2016-04-04 22:40:06 +0800

    add function to share things in detail activity

--- app/src/main/java/com/github/xzwj87/todolist/schedule/ui/activity/ScheduleDetailActivity.java
+++ app/src/main/java/com/github/xzwj87/todolist/schedule/ui/activity/ScheduleDetailActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ShareActionProvider;
 
 import com.github.xzwj87.todolist.R;
 import com.github.xzwj87.todolist.schedule.ui.fragment.ScheduleDetailFragment;
@@ -51,12 +53,37 @@ public class ScheduleDetailActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_share_schedule, menu);
+        // return true to display the share menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // share something with other social Apps
+    public void doShare(){
+        Log.v(LOG_TAG,"doShare()");
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        /* put data to it */
+        Bundle bundle = new Bundle();
+        bundle.putString("Title","I am doing something funny " +
+                "in ToDoList; come with me");
+        intent.putExtras(bundle);
+        startActivity(Intent.createChooser(intent,getResources().getText(R.string.send_to)));
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
             NavUtils.navigateUpTo(this, new Intent(this, ScheduleListActivity.class));
             return true;
+        }else if(id == R.id.menu_share){
+            doShare();
+            return false;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
