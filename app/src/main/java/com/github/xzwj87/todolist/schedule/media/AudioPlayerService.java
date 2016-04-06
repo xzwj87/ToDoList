b765a3c1a724a64f7cbe36ed2eef3fccfef3372d
JasonWang <xzwj87@gmail.com>
2016-04-06 09:15:39 +0800

    modify audio player logic for different ringer mode

--- app/src/main/java/com/github/xzwj87/todolist/schedule/media/AudioPlayerService.java
+++ app/src/main/java/com/github/xzwj87/todolist/schedule/media/AudioPlayerService.java
@@ -2,11 +2,15 @@ package com.github.xzwj87.todolist.schedule.media;
 
 
 import com.github.xzwj87.todolist.R;
+import com.github.xzwj87.todolist.schedule.ui.activity.AlarmAlertActivity;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -14,10 +18,15 @@ import android.util.Log;
  * Created by JasonWang on 2016/3/25.
  */
 
-public class AudioPlayerService extends Service{
+public class AudioPlayerService extends Service
+        implements AudioManager.OnAudioFocusChangeListener{
     private static final String TAG = "AudioPlayerService";
 
+    private AudioManager mAudioMgr;
     private MediaPlayer mPlayer;
+    private Vibrator mVibrator;
+    private int mRingerMode;
+    private long mAlarmDuration = 90*1000;
 
     @Nullable
     @Override
@@ -27,26 +36,44 @@ public class AudioPlayerService extends Service{
 
     @Override
     public void onCreate(){
-        mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.over_the_horizon);
+        Context context = getApplicationContext();
+        mAudioMgr = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+        mVibrator = (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+        mPlayer = MediaPlayer.create(context, R.raw.over_the_horizon);
     }
 
     @Override
     public int onStartCommand(Intent intent,int flags,int resId){
-        play();
+        mRingerMode = mAudioMgr.getRingerMode();
+        if(mRingerMode == AudioManager.RINGER_MODE_NORMAL) {
+            play();
+        }else if(mVibrator.hasVibrator()){
+            mVibrator.vibrate(mAlarmDuration);
+        }
+
         return super.onStartCommand(intent,flags,resId);
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
-
-        stop();
+        if(mRingerMode == AudioManager.RINGER_MODE_NORMAL) {
+            stop();
+            mAudioMgr.abandonAudioFocus(this);
+        }else if(mVibrator.hasVibrator()){
+            mVibrator.cancel();
+        }
     }
 
     protected void play(){
-        Log.d(TAG,"play()");
-        mPlayer.start();
-        mPlayer.setLooping(true);
+        Log.d(TAG, "play()");
+
+        int audioFocus = mAudioMgr.requestAudioFocus(this,AudioManager.STREAM_ALARM,
+                AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
+        if(audioFocus == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            mPlayer.start();
+            mPlayer.setLooping(true);
+        }
     }
 
     protected void stop(){
@@ -54,6 +81,21 @@ public class AudioPlayerService extends Service{
 
         if(mPlayer.isPlaying()){
             mPlayer.stop();
+        }
+        mAudioMgr.abandonAudioFocus(this);
+    }
+
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+        Log.d(TAG,"onAudioFocusChange(): changedFocus = " + focusChange);
+
+        if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT){
+            mPlayer.pause();
+        }else if(focusChange == AudioManager.AUDIOFOCUS_GAIN){
+            mPlayer.start();
+            mPlayer.setLooping(true);
+        }else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
+            stop();
         }
     }
 }
