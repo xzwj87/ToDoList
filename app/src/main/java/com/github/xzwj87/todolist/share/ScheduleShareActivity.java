54b6462be80bbf8c0a8c86c1f614e9cd5e3c2cd7
JasonWang <wlh2119@gmail.com>
2016-05-07 12:50:36 +0800

    implement SMS share function

--- app/src/main/java/com/github/xzwj87/todolist/share/ScheduleShareActivity.java
+++ app/src/main/java/com/github/xzwj87/todolist/share/ScheduleShareActivity.java
@@ -3,6 +3,7 @@ package com.github.xzwj87.todolist.share;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -14,12 +15,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.xzwj87.todolist.R;
+import com.github.xzwj87.todolist.schedule.data.provider.ScheduleContract;
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.sdk.modelmsg.WXTextObject;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.SocketHandler;
 
 /**
  * Created by JasonWang on 2016/4/30.
  */
 public class ScheduleShareActivity extends AppCompatActivity
-        implements View.OnClickListener{
+        implements View.OnClickListener,IWXAPIEventHandler{
     public static final String TAG = "ScheduleShareActivity";
 
     private PackageManager mPackageMgr;
@@ -29,8 +44,10 @@ public class ScheduleShareActivity extends AppCompatActivity
     private ImageView mShareWithSms;
     private TextView mShareCancel;
 
-    private WxShareHelper mWxShare;
     private Bundle mShareData;
+    private IWXAPI mWxAPI;
+    private int mWxShareFlag;
+    private HashMap<String,String> mAppNameMap = new HashMap<>();
 
     @Override
     public void onCreate(Bundle savedState){
@@ -39,8 +56,7 @@ public class ScheduleShareActivity extends AppCompatActivity
         Intent intent = getIntent();
         mShareData = intent.getExtras();
 
-        mWxShare = new WxShareHelper(this,mShareData);
-        mWxShare.registerToWX();
+        registerToWX();
 
         setContentView(R.layout.activity_schedule_share);
 
@@ -64,6 +80,11 @@ public class ScheduleShareActivity extends AppCompatActivity
         mShareWithWeixinCircle.setOnClickListener(this);
         mShareWithSms.setOnClickListener(this);
         mShareCancel.setOnClickListener(this);
+
+        mAppNameMap.put(ShareConstants.WX_PACKAGE_NAME,
+                getResources().getString(R.string.weixin));
+        mAppNameMap.put(ShareConstants.WEBO_PACKAGE_NAME,
+                getResources().getString(R.string.weibo));
     }
 
 
@@ -74,21 +95,20 @@ public class ScheduleShareActivity extends AppCompatActivity
         }else if(v.equals(mShareWithWeibo)){
 
         }else if(v.equals(mShareWithWeixinFriend)) {
-            if (!isInstalled(ShareConstants.WX_PACKAGE_NAME)) {
-                return;
+            mWxShareFlag = ShareConstants.SHARE_TO_FRIEND;
+            if (isInstalled(ShareConstants.WX_PACKAGE_NAME)) {
+                shareToWx();
             }
-            mShareData.putInt(ShareConstants.WX_SHARE_FLAG, ShareConstants.SHARE_TO_FRIEND);
-            mWxShare.share();
         }else if(v.equals(mShareWithWeixinCircle)){
-            if (!isInstalled(ShareConstants.WX_PACKAGE_NAME)) {
-                return;
+            mWxShareFlag = ShareConstants.SHARE_TO_CIRCLE;
+            if (isInstalled(ShareConstants.WX_PACKAGE_NAME)) {
+                shareToWx();
             }
-            mShareData.putInt(ShareConstants.WX_SHARE_FLAG, ShareConstants.SHARE_TO_CIRCLE);
-            mWxShare.share();
         }else if(v.equals(mShareWithSms)){
-
+            sendSms();
         }
-        finish();
+        // hide the UI
+        setVisible(false);
     }
 
     // whether the package is installed
@@ -99,11 +119,106 @@ public class ScheduleShareActivity extends AppCompatActivity
             return true;
         }catch (PackageManager.NameNotFoundException e){
             Log.e(TAG, packageName + "is not found");
-            Toast.makeText(this, packageName + "is not installed!",
-                    Toast.LENGTH_LONG).show();
+
+            String msg = mAppNameMap.get(packageName) + " " +
+                    getResources().getString(R.string.install_info);
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+
             e.printStackTrace();
 
             return false;
+        }
+    }
+
+    // when WX call us
+    @Override
+    public void onReq(BaseReq baseReq) {
+    }
+
+    // when we call WX, wx response
+    @Override
+    public void onResp(BaseResp resp) {
+        int resultCode = 0;
+        switch(resp.errCode){
+            case BaseResp.ErrCode.ERR_OK:
+                resultCode = R.string.share_result_sucess;
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                resultCode = R.string.share_result_cancel;
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                resultCode = R.string.share_result_auth_deny;
+                break;
+            default:
+                resultCode = R.string.share_result_unknown;
+                break;
+        }
+        Toast.makeText(this,resultCode,Toast.LENGTH_LONG).show();
+        // delay to finish the activity
+        finish();
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 1000);*/
+    }
+
+
+    public void shareToWx(){
+        Log.v(TAG,"shareToWx");
+
+        long  scheduleTime = mShareData.getLong(ScheduleContract.ScheduleEntry.COLUMN_ALARM_TIME);
+        String scheduleTitle = mShareData.getString(ScheduleContract.ScheduleEntry.COLUMN_TITLE);
+        String format = "yyyy/MM/dd,HH:mm";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        String msg = dateFormat.format(new Date(scheduleTime)) + ",我在Todo List完成了任务"
+                + "#" + scheduleTitle +"#" + ",快来加入吧!";
+        WXTextObject textObject = new WXTextObject();
+        textObject.text = msg;
+
+        WXMediaMessage mediaMessage = new WXMediaMessage();
+        mediaMessage.mediaObject = textObject;
+        mediaMessage.description = scheduleTitle;
+
+        // send the req to weixin
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = String.valueOf(System.currentTimeMillis());
+        req.message = mediaMessage;
+        req.scene = mWxShareFlag == 0x00 ? SendMessageToWX.Req.WXSceneSession :
+                SendMessageToWX.Req.WXSceneTimeline;
+
+        mWxAPI.sendReq(req);
+    }
+
+    // register Tencent WX API
+    public void registerToWX(){
+        mWxAPI = WXAPIFactory.createWXAPI(this, ShareConstants.WX_APP_ID, true);
+        mWxAPI.registerApp(ShareConstants.WX_APP_ID);
+    }
+
+
+    public void sendSms(){
+
+        String scheduleTitle = mShareData.getString(ScheduleContract.ScheduleEntry.COLUMN_TITLE);
+        String msg = "#Todo List#" + "我刚添加了新的任务:"  + scheduleTitle +"，快来加入吧！";
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setType("vnd.android-dir/mms-sms");
+        intent.putExtra(ShareConstants.SMS_BODY, msg);
+        startActivityForResult(intent, ShareConstants.SHARE_TO_SMS);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == ShareConstants.SHARE_TO_SMS){
+            int msgId = 0;
+            if(resultCode == RESULT_OK){
+                msgId = R.string.share_result_sucess;
+            }else if(resultCode == RESULT_CANCELED){
+                msgId = R.string.share_result_cancel;
+            }
+            Toast.makeText(this,msgId,Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/github/xzwj87/todolist/share/ShareConstants.java
+++ app/src/main/java/com/github/xzwj87/todolist/share/ShareConstants.java
@@ -1,13 +1,19 @@
 package com.github.xzwj87.todolist.share;
 
+import java.util.HashMap;
+
 /**
  * Created by JasonWang on 2016/4/30.
  */
 public interface ShareConstants {
 
-    public static final String WX_SHARE_FLAG = "wx_share_flag";
+    public static final String WEBO_PACKAGE_NAME = "com.sina.weibo";
     public static final String WX_PACKAGE_NAME = "com.tencent.mm";
     public static final String WX_APP_ID = "wxb6f7522430205227";
+
+    public static final String SMS_BODY = "sms_body";
+
     public static final int SHARE_TO_FRIEND = 0x00;
     public static final int SHARE_TO_CIRCLE = 0x01;
+    public static final int SHARE_TO_SMS = 0x02;
 }
